d0c62a70ea1d7b1767cf45e09856a65668c0c092
fk12593 <fk12593@163.com>
2013-10-16 11:01:12 +0800

    this is my work

---
+++ html/divcss/js/cf.js
@@ -0,0 +1,18 @@
+function displayzj(flag){
+	var ele = document.getElementById("zhoujie");
+	var elec = document.getElementById("dazhoujie");
+    if(flag==1){
+    	elec.style.display = "block";
+    }else{     	
+		   elec.style.display = "none";
+		}                                                                                             
+} 
+
+
+$(function(){
+	initEvents();
+});
+
+function initEvents(){
+	$
+}
---
+++ html/divcss/js/jsdemo.js
@@ -0,0 +1,10 @@
+
+$(function(){
+  //事件委托
+  initEleEvents();
+  
+});
+
+function initEleEvents(){
+  
+}
